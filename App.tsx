03e465261634da1633cc4b94e5580946a1af1ba4
alfxly <160280212+alfxly@users.noreply.github.com>
2024-09-09 11:29:10 -0500

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Index from "./components/Index";
+
+function App() {
+  return (
+    <div>
+      <Index />
+    </div>
+  );
+}
+
+export default App;
